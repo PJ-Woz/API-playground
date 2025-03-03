00e5adf89057d9603b91f8b6519a8f845ec1374c
PJ-Woz <118623111+PJ-Woz@users.noreply.github.com>
2025-03-03 19:46:40 +0100

    Add openapi.json

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -6,6 +6,6 @@ export class TestController {
     // Method to handle GET requests to the /test endpoint
     public getTest(req: Request, res: Response): void {
         // Sending a simple JSON response with a message
-        res.json({ message: 'This is a test response' });
+        res.json({ message: 'Hello' });
     }
 }
